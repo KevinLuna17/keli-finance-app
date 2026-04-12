ab2d09fc5b45666b2426bce2dc4d1748b84e80bc
KevinLuna17 <kelugo_14@hotmail.com>
2026-04-12 04:02:27 -0400

    feat: add "forgot-password" screen to authentication flow; enhance sign-in screen with improved input handling and visual feedback

--- src/app/(auth)/_layout.tsx
+++ src/app/(auth)/_layout.tsx
@@ -10,6 +10,7 @@ export default function AuthRoutesLayout() {
     >
       <Stack.Screen name="sign-in" options={{ headerShown: false }} />
       <Stack.Screen name="sign-up" options={{ headerShown: false }} />
+      <Stack.Screen name="forgot-password" options={{ headerShown: false }} />
     </Stack>
   );
 }
--- src/app/(auth)/sign-in.tsx
+++ src/app/(auth)/sign-in.tsx
@@ -1,20 +1,29 @@
 import useSocialAuth from "@/hooks/useSocialAuth";
 import { Image } from "expo-image";
-import { SymbolView } from "expo-symbols";
 import * as React from "react";
-import { Pressable, StyleSheet, Text, TextInput, View } from "react-native";
+import {
+  Pressable,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 import { useSignIn } from "@clerk/expo";
 import { FontAwesome, FontAwesome6 } from "@expo/vector-icons";
-import { type Href, useRouter } from "expo-router";
+import { type Href, Link, useRouter } from "expo-router";
 
 export default function SignInScreen() {
   const { signIn, errors, fetchStatus } = useSignIn();
   const router = useRouter();
 
   const [emailAddress, setEmailAddress] = React.useState("");
+  const [emailFocused, setEmailFocused] = React.useState(false);
   const [password, setPassword] = React.useState("");
+  const [showPassword, setShowPassword] = React.useState(false);
+  const [passwordFocused, setPasswordFocused] = React.useState(false);
   const [code, setCode] = React.useState("");
 
   const [error, setError] = React.useState("");
@@ -174,7 +183,7 @@ export default function SignInScreen() {
         <View className="mt-6 rounded-[30px] border border-white/20 bg-white/10 p-3">
           <Image
             source={require("../../../assets/images/keli-logo.png")}
-            style={{ width: "100%", height: 200 }}
+            style={{ width: "100%", height: 180 }}
             contentFit="contain"
           />
         </View>
@@ -194,45 +203,94 @@ export default function SignInScreen() {
 
         <View className="mt-6">
           {/* Email Input*/}
-          <TextInput
-            placeholder="Email address"
-            value={emailAddress}
-            onChangeText={(emailAddress) => setEmailAddress(emailAddress)}
-            autoCapitalize="none"
-            keyboardType="email-address"
-            autoComplete="email"
-            className="border border-border rounded-2xl p-4 mb-3"
-            placeholderTextColor="#666666"
-          />
+          <View
+            className={`flex-row items-center h-14 border border-border rounded-2xl px-4 mb-3 ${
+              emailFocused ? "border-2 border-primary" : "border-border"
+            }`}
+          >
+            <View className="w-12 h-full justify-center items-center">
+              <FontAwesome6
+                name="envelope"
+                size={18}
+                color={emailFocused ? "#508A67" : "#5f6e66"}
+              />
+            </View>
+            <TextInput
+              className="flex-1 text-base text-card-foreground"
+              placeholder="Email address"
+              value={emailAddress}
+              onChangeText={(emailAddress) => setEmailAddress(emailAddress)}
+              onFocus={() => setEmailFocused(true)}
+              onBlur={() => setEmailFocused(false)}
+              autoCapitalize="none"
+              keyboardType="email-address"
+              autoComplete="email"
+              placeholderTextColor="#5f6e66"
+            />
+          </View>
           {errors.fields.identifier && (
             <Text className="text-red-500">
               {errors.fields.identifier.message}
             </Text>
           )}
           {/* Password Input*/}
-          <TextInput
-            className="border border-border rounded-2xl p-4 mb-3"
-            value={password}
-            placeholder="Enter password"
-            placeholderTextColor="#666666"
-            secureTextEntry={true}
-            onChangeText={(password) => setPassword(password)}
-          />
+          <View
+            className={`flex-row items-center h-14 border border-border rounded-2xl px-4 mb-3 ${
+              passwordFocused ? "border-2 border-primary" : "border-border"
+            }`}
+          >
+            <View className="w-12 h-full justify-center items-center">
+              <FontAwesome6
+                name="lock"
+                size={18}
+                color={passwordFocused ? "#508A67" : "#5f6e66"}
+              />
+            </View>
+            <TextInput
+              className="flex-1 text-base text-card-foreground"
+              value={password}
+              placeholder="Enter password"
+              placeholderTextColor="#5f6e66"
+              secureTextEntry={!showPassword}
+              onChangeText={(password) => setPassword(password)}
+              onFocus={() => setPasswordFocused(true)}
+              onBlur={() => setPasswordFocused(false)}
+            />
+            <Pressable
+              onPress={() => setShowPassword(!showPassword)}
+              className="w-14 h-full justify-center items-center"
+            >
+              <FontAwesome
+                name={showPassword ? "eye-slash" : "eye"}
+                size={18}
+                color={passwordFocused ? "#508A67" : "#5f6e66"}
+              />
+            </Pressable>
+          </View>
           {errors.fields.password && (
             <Text className="text-red-500">
               {errors.fields.password.message}
             </Text>
           )}
 
+          {/* Forgot Password */}
+          <Link href="/(auth)/forgot-password" asChild>
+            <TouchableOpacity className="self-end">
+              <Text className="text-base text-muted-foreground font-bold">
+                Forgot password?
+              </Text>
+            </TouchableOpacity>
+          </Link>
+
           {/* Error Message */}
           {error ? (
             <View className="flex-row items-center rounded-2xl border border-destructive bg-destructive/10 px-4 py-2">
-              <SymbolView
-                name="exclamationmark.triangle.fill"
+              <FontAwesome
+                name="exclamation-triangle"
                 size={16}
-                tintColor="#DC2626"
+                color="#DC2626"
               />
-              <Text selectable className="text-red-500">
+              <Text selectable className="ml-2 flex-1 text-red-500">
                 {error}
               </Text>
             </View>
@@ -252,51 +310,68 @@ export default function SignInScreen() {
               {fetchStatus === "fetching" ? "Signing in..." : "Continue"}
             </Text>
 
-            <FontAwesome name="angle-right" size={18} color="#5f6e66" />
+            <FontAwesome name="angle-right" size={18} color="#fff" />
           </Pressable>
         </View>
 
+        <Text className="mt-3 text-center text-base leading-6 text-muted-foreground">
+          Don't have an account?{" "}
+          <Link href="/(auth)/sign-up" asChild>
+            <Text className="text-muted-foreground text-base font-bold">
+              Sign up
+            </Text>
+          </Link>
+        </Text>
+
         <View className="mt-6">
-          {/* Google button */}
-          <Pressable
-            className={`mb-3 h-14 flex-row items-center rounded-2xl border border-border bg-card px-4 active:opacity-90 ${
-              isLoading ? "opacity-70" : ""
-            }`}
-            disabled={isLoading}
-            onPress={() => handleSocialAuth("oauth_google")}
-          >
-            <View className="h-8 w-8 items-center justify-center rounded-full bg-white">
-              <Image
-                source={require("../../../assets/images/google.png")}
-                style={{ width: 20, height: 20 }}
-              />
-            </View>
-
-            <Text className="ml-3 flex-1 text-lg font-semibold text-card-foreground">
-              {isGoogleClicked
-                ? "Connecting Google..."
-                : "Continue with Google"}
+          <View className="flex-row items-center">
+            <View className="h-px flex-1 bg-border" />
+            <Text className="px-4 text-sm leading-5 text-muted-foreground">
+              Or login with
             </Text>
+            <View className="h-px flex-1 bg-border" />
+          </View>
 
-            <FontAwesome name="angle-right" size={18} color="#5f6e66" />
-          </Pressable>
+          {/* Social login buttons */}
+          <View className="flex-row justify-center items-center gap-4 mt-6">
+            {/* Google button */}
+            <Pressable
+              className={`mb-3 w-52 h-14 flex-row items-center rounded-2xl border border-border bg-card px-4 active:opacity-90 ${
+                isLoading ? "opacity-70" : ""
+              }`}
+              disabled={isLoading}
+              onPress={() => handleSocialAuth("oauth_google")}
+            >
+              <View className="h-8 w-8 items-center justify-center rounded-full bg-white">
+                <Image
+                  source={require("../../../assets/images/google.png")}
+                  style={{ width: 20, height: 20 }}
+                />
+              </View>
 
-          {/* Apple button */}
-          <Pressable
-            className={`mb-3 h-14 flex-row items-center rounded-2xl border border-border bg-card px-4 active:opacity-90 ${
-              isLoading ? "opacity-70" : ""
-            }`}
-            disabled={isLoading}
-            onPress={() => handleSocialAuth("oauth_apple")}
-          >
-            <View className="h-8 w-8 items-center justify-center rounded-full bg-white">
-              <FontAwesome6 name="apple" size={22} color="#111" />
-            </View>
-            <Text className="ml-3 flex-1 text-lg font-semibold text-card-foreground">
-              {isAppleClicked ? "Connecting Apple..." : "Continue with Apple"}
-            </Text>
-            <FontAwesome name="angle-right" size={18} color="#5f6e66" />
-          </Pressable>
+              <Text className="ml-3 flex-1 text-lg font-semibold text-card-foreground">
+                {isGoogleClicked ? "Connecting..." : "Google"}
+              </Text>
+              <FontAwesome name="angle-right" size={18} color="#5f6e66" />
+            </Pressable>
+
+            {/* Apple button */}
+            <Pressable
+              className={`mb-3 w-52 h-14 flex-row items-center rounded-2xl border border-border bg-card px-4 active:opacity-90 ${
+                isLoading ? "opacity-70" : ""
+              }`}
+              disabled={isLoading}
+              onPress={() => handleSocialAuth("oauth_apple")}
+            >
+              <View className="h-8 w-8 items-center justify-center rounded-full bg-white">
+                <FontAwesome6 name="apple" size={22} color="#111" />
+              </View>
+              <Text className="ml-3 flex-1 text-lg font-semibold text-card-foreground">
+                {isAppleClicked ? "Connecting..." : "Apple"}
+              </Text>
+              <FontAwesome name="angle-right" size={18} color="#5f6e66" />
+            </Pressable>
+          </View>
         </View>
 
         <Text className="mt-3 text-center text-sm leading-5 text-muted-foreground">
