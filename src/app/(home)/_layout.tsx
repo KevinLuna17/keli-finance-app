512ca8b7143fd8aa9bfc8cc7de4c8601c31a8e6c
kevinlu <kelugo_14@hotmail.com>
2026-05-25 15:17:49 -0400

    fix: correct return statement in Layout component to properly render Tabs and Screens

--- src/app/(home)/_layout.tsx
+++ src/app/(home)/_layout.tsx
@@ -2,20 +2,22 @@ import { FontAwesome } from "@expo/vector-icons";
 import { Tabs } from "expo-router";
 
 export default function Layout() {
-  <Tabs
-    screenOptions={{
-      tabBarActiveTintColor: "bg-primary",
-      headerShown: false,
-    }}
-  >
-    <Tabs.Screen
-      name="index"
-      options={{
-        title: "Home",
-        tabBarIcon: ({ color }) => (
-          <FontAwesome size={28} name="home" color={color} />
-        ),
+  return (
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: "bg-primary",
+        headerShown: false,
       }}
-    />
-  </Tabs>;
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Home",
+          tabBarIcon: ({ color }) => (
+            <FontAwesome size={28} name="home" color={color} />
+          ),
+        }}
+      />
+    </Tabs>
+  );
 }
